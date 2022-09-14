5d0eff3fd6b71382544ae65b3ca1a44d9cf4fd05
Niki_Manani <nikisuccess.deore402@gmail.com>
2022-09-14 21:34:17 +0530

    Updates

--- module1.mjs
+++
@@ -1,5 +0,0 @@
-const a='niki';
-const b='mehul';
-const c='nikmeh😊';
-export default  c;
-export {a,b};
--- module2.mjs
+++
@@ -1,4 +0,0 @@
-import ui,{a,b} from './module1.mjs';
-
-console.log(ui);
-console.log(a,b);
